085d833815cc84d42d5d4eac8d0381c211376d94
Tommy <thomasfischer1337@gmail.com>
2021-07-26 12:45:33 +0200

    Update files after changes

--- app.js
+++ app.js
@@ -1,60 +1,60 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var path = require("path")
-var db = require('./db');
-
+var path = require("path");
+var db = require("./db");
 
 app.use(function (req, res, next) {
-  const allowedOrigins = ['http://localhost:5000', 'http://localhost:3000', 'https://tommycodes-blog.herokuapp.com'];
+  const allowedOrigins = [
+    "http://localhost:5000",
+    "http://localhost:3000",
+    "https://tommycodes-blog.herokuapp.com",
+  ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+  next();
 });
 
+app.use(express.static(path.join(__dirname, "build")));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.get('/profile', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/profile", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.get('/userManagement', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/userManagement", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-var UserController = require('./user/UserController');
-app.use('/api/users', UserController);
+var UserController = require("./user/UserController");
+app.use("/api/users", UserController);
 
-var AuthController = require('./auth/AuthController');
-app.use('/api/auth', AuthController);
+var AuthController = require("./auth/AuthController");
+app.use("/api/auth", AuthController);
 
-var ProfileController = require('./profile/ProfileController');
-app.use('/api/profile', ProfileController);
+var ProfileController = require("./profile/ProfileController");
+app.use("/api/profile", ProfileController);
 
-var EntryController = require('./blog/entry/EntryController');
-app.use('/api/entry', EntryController);
+var EntryController = require("./blog/entry/EntryController");
+app.use("/api/entry", EntryController);
 
 module.exports = app;
