20d0c2b8a7cdf194a74b431dc0c61aacdcaf2543
Thomas Fischer <thomasfischer1337@gmail.com>
2022-01-17 18:10:23 +0100

    bugfix: remove log

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var app = require('./app');
 require('dotenv').config();
 var port = process.env.PORT || 3001;
 
-console.log("the port!:", process.env.PORT)
-
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
