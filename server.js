8354d06bc52e7392c2646853281d4fc1a9c05f41
Thomas Fischer <thomasfischer1337@gmail.com>
2022-01-17 17:55:18 +0100

    feat: add dotenv to serverjs

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var app = require('./app');
+require('dotenv').config();
 var port = process.env.PORT || 3001;
+
+console.log("the port!:", process.env.PORT)
 
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
